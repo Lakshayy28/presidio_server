bb366e456d1295865052c97dce105ac686525da6
Lakshay Chandra <lakshaychandra28@gmail.com>
2026-04-01 01:15:31 +0530

    chore: update build artifact metadata (auto-generated)

--- bdd/fixtures/file_samples/obj/Debug/net8.0/sample.AssemblyInfo.cs
+++ bdd/fixtures/file_samples/obj/Debug/net8.0/sample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb27d572e4be9002c1b337e1864d4fa42053ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28fcfe3786221f46b92ac5af7cf6c8de4259ccdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
